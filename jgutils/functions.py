b7985d2e32796218442cbc9f14071311543b3716
Jayme Gordon <jayme_gordon@icloud.com>
2022-11-02 20:13:08 -0600

    Add magenta

--- jgutils/functions.py
+++ jgutils/functions.py
@@ -314,6 +314,7 @@ class PrettyString():
             'green': '\033[32m',
             'yellow': '\033[33m',
             'blue': '\033[34m',
+            'magenta': '\033[35m',
             'red': '\033[31m',
             'cyan': '\033[36m',
             'reset': '\033[0m'
