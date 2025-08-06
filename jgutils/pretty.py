d672d007543be424a6f3aa1f36aa69703e4705b2
Jayme Gordon <jayme_gordon@icloud.com>
2025-08-05 20:10:56 -0700

    add pydantic support to PrettyDict

--- jgutils/pretty.py
+++ jgutils/pretty.py
@@ -1,5 +1,15 @@
 import re
 from abc import ABCMeta
+from typing import TYPE_CHECKING
+from typing import Any
+from typing import Union
+
+from pydantic import BaseModel
+
+if TYPE_CHECKING:
+    from jgutils.typing import Listable
+
+PrettyType = dict | BaseModel | list | tuple | set
 
 
 class PrettyDisplayItem(metaclass=ABCMeta):  # noqa: B024
@@ -44,11 +54,12 @@ class PrettyDict(PrettyDisplayItem):
             param: str = '  ',
             max_keys: int = 100,
             max_rows: int = 100,
+            max_lines: int | None = None,
             color: bool = True):
         """
         Parameters
         ----------
-        m : dict | list[dict]
+        m : Union[DictAny, list[DictAny]]
             dict or list of dicts to display
         param : str, optional
             beginning string that gets multiplied at each depth level, by default '  '
@@ -63,27 +74,113 @@ class PrettyDict(PrettyDisplayItem):
         self.param = param
         self.max_keys = max_keys
         self.max_rows = max_rows
+        self.max_lines = max_lines
 
         # list ansi escape codes for each level of depth
         self.ansi_codes_list = list(self.ansi_codes.values())[:-1] * 3
 
         # don't highlight keys, just use structural formatting only
-        from jgutils.config import IS_REMOTE
+        from jambot.config import IS_REMOTE
         if not color or IS_REMOTE:
             self.ansi_codes_list = [''] * len(self.ansi_codes_list)
             self.reset = ''
         else:
             self.reset = self.ansi_codes['reset']
 
+    @classmethod
+    def from_attrs(
+            cls,
+            obj: 'Listable[Any]',
+            attrs: 'Listable[str]',
+            key: str | None = None,
+            **kw) -> 'PrettyDict':
+        """Create PrettyDict from attributes of an object
+        - if obj is iterable, create PD from list of each, else single dict
+
+        Parameters
+        ----------
+        obj : Listable[Any]
+            single or iterable object(s)
+        attrs : Listable[str]
+            attributes to display
+        key : str | None, optional
+            key to use for each item, by default int index
+        """
+        from jgutils.utils import as_list
+
+        attrs = as_list(attrs)
+
+        data = {}
+        for i, _obj in enumerate(as_list(obj)):
+            _key = getattr(_obj, key) if key else i
+            data[_key] = {attr: getattr(_obj, attr) for attr in attrs}
+
+        if len(data) == 1:
+            data = list(data.values())[0]
+
+        return cls(data, **kw)
+
     def __str__(self) -> str:
         return self.pretty_print(self.m)
 
-    def pretty_print(self, m: dict | list[dict], depth: int = 0) -> str:
+    def truncate_item(self, item: Any) -> Any:  # noqa: ANN401
+        """Truncate string or iterable to max_lines"""
+        if not self.max_lines:
+            return item
+
+        if isinstance(item, str):
+            item = '\n'.join(item.split('\n')[:self.max_lines])
+        elif isinstance(item, list | tuple):
+            item = item[:self.max_lines]
+        elif isinstance(item, set):
+            item = list(item)[:self.max_lines]
+
+        return item
+
+    def _format_iterable(self, items: list | tuple | set, depth: int) -> str:
+        """Format an iterable with consistent indentation and truncation
+
+        Parameters
+        ----------
+        items : list | tuple | set
+            Iterable to format
+        depth : int
+            Current indentation depth
+
+        Returns
+        -------
+        str
+            Formatted string representation
+        """
+        ret = '\n'
+        i_list = 0
+        depth_indent = depth * self.param
+
+        for item in items:
+
+            if i_list >= self.max_rows:
+                ret += f'{depth_indent}... ({self.max_rows:,.0f}/{len(items):,.0f})\n'
+                break
+
+            if isinstance(item, dict | BaseModel):
+                if isinstance(item, BaseModel):
+                    item = item.model_dump()
+                ret += self.pretty_print(item, depth + 1) + '\n'
+            elif isinstance(item, list | tuple | set):
+                ret += self._format_iterable(item, depth + 1)[:-1] + '\n'  # Remove trailing newline from recursive call
+            else:
+                ret += f'{depth_indent}{item}\n'
+
+            i_list += 1  # noqa: SIM113
+
+        return ret
+
+    def pretty_print(self, m: PrettyType, depth: int = 0) -> str:
         """Recursively pretty print nested dicts with keys colored by depth
 
         Parameters
         ----------
-        m : dict | list[dict]
+        m : PrettyType
             nested dict or list of dicts to pretty print
         depth : int, optional
             depth of current dict, default 0
@@ -98,19 +195,19 @@ class PrettyDict(PrettyDisplayItem):
         lst = self.ansi_codes_list
         reset = self.reset
         i_dict = 0
-        i_list = 0
 
-        if isinstance(m, dict):
+        if isinstance(m, dict | BaseModel):
+            if isinstance(m, BaseModel):
+                m = m.model_dump()
+
             for k, v in m.items():
-
-                if isinstance(v, dict):
+                if isinstance(v, dict | BaseModel):
                     ret += f'{depth_indent}{lst[depth]}{k}{reset}:\n{self.pretty_print(v, depth + 1)}'
                 else:
-                    # join list of dicts into single value
-                    if isinstance(v, list | tuple):
-                        v = '\n' + \
-                            '\n'.join([
-                                self.pretty_print(item, depth + 1) for item in v])
+                    v = self.truncate_item(v)
+
+                    if isinstance(v, list | tuple | set):
+                        v = self._format_iterable(v, depth + 1)
 
                     ret += f'{depth_indent}{lst[depth]}{k}{reset}: {v}\n'
 
@@ -119,17 +216,8 @@ class PrettyDict(PrettyDisplayItem):
                     ret += f'{depth_indent}...\n'
                     break
 
-        elif isinstance(m, list | tuple):
-            for item in m:
-                if isinstance(item, dict):
-                    ret += self.pretty_print(item, depth + 1) + '\n'
-                else:
-                    ret += f'{depth_indent}{item}\n'
-
-                i_list += 1
-                if i_list >= self.max_rows:
-                    ret += f'{depth_indent}... ({self.max_rows:,.0f}/{len(m):,.0f})\n'
-                    break
+        elif isinstance(m, list | tuple | set):
+            ret = self._format_iterable(m, depth)[:-1]  # Remove trailing newline
 
         else:
             ret += f'{depth_indent}{m}'
@@ -145,7 +233,7 @@ class PrettyString(PrettyDisplayItem):
             s: str,
             color: str = 'green',
             prehighlight: bool = False,
-            expr: str | re.Pattern[str] | None = None):
+            expr: Union[str, 're.Pattern[str]'] | None = None):
         """
         Parameters
         ----------
@@ -155,7 +243,7 @@ class PrettyString(PrettyDisplayItem):
             color to use, default 'green'
         prehighlight : bool, optional
             string already contains color codes, just display it, default False
-        expr : str | re.Pattern[str], optional
+        expr : Optional[Union[str, re.Pattern[str]]], optional
             regex expression to highlight specific substrings, default None
         """
         self.s = s
@@ -199,7 +287,7 @@ class PrettyString(PrettyDisplayItem):
         str
             string with ansi escape code colors
         """
-        from jgutils.config import IS_REMOTE
+        from jambot.config import IS_REMOTE
         if IS_REMOTE:
             return str(s)
 
@@ -212,8 +300,7 @@ class PrettyString(PrettyDisplayItem):
                 if not match_str.strip() == '':
                     # replace the match with the highlighted version, using the match start and end
                     color_replace = self._wrap_highlight(match_str)
-                    s = s[:match.start() + offset] + color_replace + \
-                        s[match.end() + offset:]
+                    s = s[:match.start() + offset] + color_replace + s[match.end() + offset:]
 
                     offset += len(color_replace) - len(match_str)
 
